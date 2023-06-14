dc40bbc15963d408777cd1070489902b1dab81ac
dahee oh <ekgml4122@gmail.com>
2023-06-14 23:50:53 +0900

    나도자바 continue 공부

---
+++ src/chap_04/_11_Continue.java
@@ -0,0 +1,45 @@
+package chap_04;
+
+public class _11_Continue {
+    public static void main(String[] args) {
+        // for문
+        int max = 20;  // 최대 치킨 판매 수량
+        int sold = 0;  // 현재 치킨 판매 수량
+        int noShow = 17;  // 대기번호 17번 손님이 노쇼
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i + "번 손님, 주문하신 치킨 나왔습니다.");
+
+            // 노쇼 고객이 있다면?
+            if (i == noShow) {
+                System.out.println(i + "번 손님, 노쇼로 인해 다음 손님에게 넘어갑니다.");
+                continue;
+            }
+
+            sold++;
+            if (sold == max)  {
+                System.out.println("금일 재료가 모두 소진되었습니다.");
+                break;
+            }
+        }
+        System.out.println("영업을 종료합니다.");
+
+
+        // while문
+        int index = 0;  // 손님 번호
+        sold = 0;  // sold 초기화
+        while (index < 50) {
+            index++;
+            System.out.println(index + "번 손님, 주문하신 치킨 나왔습니다.");
+            if (index == noShow) {
+                System.out.println(index + "번 손님, 노쇼로 인해 다음 손님에게 넘어갑니다.");
+                continue;
+            }
+            sold++;  // 판매 처리
+            if (sold == max) {
+                System.out.println("금일 재료가 모두 소진되었습니다.");
+                break;
+            }
+        }
+        System.out.println("영업을 종료합니다.");
+    }
+}
---
+++ src/chap_04/_Quiz_04.java
@@ -0,0 +1,2 @@
+package chap_04;public class _Quiz_04 {
+}
