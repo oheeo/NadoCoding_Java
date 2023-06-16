e7501a064fc6851e9b74ecc2afbd033d869ec07f
dahee oh <ekgml4122@gmail.com>
2023-06-16 23:38:50 +0900

    다차원 배열(2차원 배열)

---
+++ src/chap_05/_03_MultiArray.java
@@ -0,0 +1,27 @@
+package chap_05;
+
+public class _03_MultiArray {
+    public static void main(String[] args) {
+        // 다차원 배열 (2차원 배열)
+
+        // 소규모 영화관 좌석
+        // A1 - A5
+        // B1 - B2
+        // C1 - C2
+
+        String[] seatA = {"A1", "A2", "A3", "A4", "A5"};
+        String[] seatB = {"B1", "B2", "B3", "B4", "B5"};
+        String[] seatC = {"C1", "C2", "C3", "C4", "C5"};
+
+        // 3(행) x 5(열) 크기의 2차원 배열
+        String[][] seats = new String[][] {
+                {"A1", "A2", "A3", "A4", "A5"},
+                {"B1", "B2", "B3", "B4", "B5"},
+                {"C1", "C2", "C3", "C4", "C5"}
+        };
+
+        // C2 에 접근하려면?  (0부터 시작)
+        System.out.println(seats[2][1]);
+        System.out.println(seats[1][3]);  // B4
+    }
+}
