83d7f56d92564fb7cd8b0e3c5209753c49dd2fcc
dahee oh <ekgml4122@gmail.com>
2023-06-17 23:59:18 +0900

    method

---
+++ src/chap_06/_01_Method.java
@@ -0,0 +1,16 @@
+package chap_06;
+
+public class _01_Method {
+    // 메소드 정의
+    public static void sayHello() {
+        System.out.println("안녕하세요? 메소드 입니다.");
+    }
+
+    public static void main(String[] args) {
+        // 메소드 호출
+        sayHello();    // "안녕하세요? 메소드 입니다." 출력
+        sayHello();
+        sayHello();
+
+    }
+}
