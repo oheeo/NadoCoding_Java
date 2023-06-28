f5f3a9ad3d1f5a9725c10dcccefd68f4301e4e1b
Hee <122732781+oheeo@users.noreply.github.com>
2023-06-28 23:34:44 +0900

    return
    메소드의 반환값 return

---
+++ src/chap_06/_03_Return.java
@@ -0,0 +1,31 @@
+package chap_06;
+
+public class _03_Return {
+    // 호텔 전화번호
+    public static String getPhoneNumber() {
+        // ↑ 반환하는 값과 같은 타입인 String (void 는 반환값이 없을때)
+        String phoneNumber = "02-1234-5678";
+        return phoneNumber;
+    }
+
+    // 호텔 주소
+    public static String getAddress() {
+        return "서울시 어딘가";  // 값 자체를 리턴할 수도 있다
+    }
+
+    // 호텔 엑티비티
+    public static String getActivities() {
+        return "볼링장, 탁구장, 노래발";
+    }
+
+    public static void main(String[] args) {
+        // 반환값 return
+        String contactNumber = getPhoneNumber();  // 메소드 호출 후 반환되는 값을 받아서 뭔가들 해야겠지? 그러려면 변수로 선언
+        System.out.println("호텔 전화번호 : " + contactNumber);
+
+        String address = getAddress();
+        System.out.println("호텔 주소 : " + address);
+
+        System.out.println("호텔 액티비티 : " + getActivities());  // 메소드를 바로 쓸 수도 있다
+    }
+}
