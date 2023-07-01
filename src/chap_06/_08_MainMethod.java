aabb1308d25f3128f256263c3e97f1a0faa05c3f
dahee oh <ekgml4122@gmail.com>
2023-07-01 23:00:45 +0900

    Nado Cording-Java Main Method

--- src/chap_06/_08_MainMethod.java
+++ src/chap_06/_08_MainMethod.java
@@ -7,9 +7,6 @@ public class _08_MainMethod {
             System.out.println(s);
         }
 
-        // 1. 도서 조회
-        // 2. 도서 대출
-        // 3. 도서 반납
         if (args.length == 1) {
             switch (args[0]) {
                 case "1":
