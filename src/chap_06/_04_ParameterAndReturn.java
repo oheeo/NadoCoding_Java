e85ae8cd3f5395a4f836717e5e8c397631467fc5
dahee oh <ekgml4122@gmail.com>
2023-07-12 23:09:32 +0900

    전달값과 반환값이 있는 메소드

--- src/chap_06/_04_ParameterAndReturn.java
+++ src/chap_06/_04_ParameterAndReturn.java
@@ -1,11 +1,25 @@
 package chap_06;
 
 public class _04_ParameterAndReturn {
+
+//    public static void power(int number) {  // Parameter 매개변수
+//        int result = number * number;
+//        System.out.println(number + " 의 2 승은 " + result);
+//    }
+
     public static int getPower(int number) {
         int result = number * number;
         return result;
-        // return number * number  // 이렇게 해도 됨
+        // return number * number;
     }
+//
+//    public static void powerByExp(int number, int exponent) {
+//        int result = 1;
+//        for (int i = 0; i < exponent; i++) {
+//            result *= number;
+//        }
+//        System.out.println(number + " 의 " + exponent + " 승은 " + result);
+//    }
 
     public static int getPowerByExp(int number, int exponent) {
         int result = 1;
@@ -16,8 +30,8 @@ public class _04_ParameterAndReturn {
     }
 
     public static void main(String[] args) {
-        // 전달값과 반환값이 있는 메소드
-        int retVal = getPower(2);
+        // 전달값과 반환값이 있는 메소드 호출
+        int retVal = getPower(2);  // getPower return값을 받기 위한 변수 retVal 정의
         System.out.println(retVal);  // 2 * 2 = 4
 
         retVal = getPower(3);
