6d99ee26a99b41ed212cf8d934344b0d6808dcab
dahee oh <ekgml4122@gmail.com>
2023-07-13 00:03:39 +0900

    메소드가 필요한 이유

--- src/chap_06/_06_WhenToUse.java
+++ src/chap_06/_06_WhenToUse.java
@@ -2,6 +2,14 @@ package chap_06;
 
 public class _06_WhenToUse {
 
+    public static int getPower(int number) {
+        // int result = number * number;  // number의 제곱을 구하는 식
+        // return result;
+        // 이럴 필요 없이 아래의 getPower() 메소드를 이용해서 제곱을 구할 수 있다.
+        // number 를 2 만큼 반복해서 곱하면 그게 바로 제곱
+        return getPower(number, 2);
+    }
+
     public static int getPower(int number, int exponent) {
         int result = 1;
         for (int i = 0; i < exponent; i++) {  // number 을 exponent 만큼 반복해서 곱한다
