43df59db107d578b1070a86dfe1ee3a37df929a6
dahee oh <ekgml4122@gmail.com>
2023-07-13 00:17:39 +0900

    Method Overloading

--- src/chap_06/_05_Overloading.java
+++ src/chap_06/_05_Overloading.java
@@ -12,8 +12,8 @@ public class _05_Overloading {
 //        return number * number;
 //    }
     // 문자열로 받아도 number * number 을 쓸 수 있게 됐다.
-    // 그러나 매개변수의 타입에 따라 메소드를 여러개 만들면 혼란이 올 수 있다.
-    // 이럴땐 메소드 오버로딩! 메소드명을 동일하게 해주면 된다.
+    // 그러나 매개변수의 타입에 따라 메서드를 여러개 만들면 혼란이 올 수 있다.
+    // 이럴땐 메서드 오버로딩! 메서드명을 동일하게 해주면 된다.
     public static int getPower(String strNumber) {
         int number = Integer.parseInt(strNumber);
         return number * number;
@@ -36,8 +36,8 @@ public class _05_Overloading {
         System.out.println(getPower(3));  // 3 * 3 = 9
         System.out.println(getPower("4"));  // 4 * 4 = 16
         System.out.println(getPower(3, 3));  // 3 * 3 * 3 = 27
-        // 전달되는 자료형에 따라 메소드 이름은 똑같지만 전달값이 다른 메소드를 각각 호출함
-        // 메소드 오버로딩 : 같은 이름의 메소드를 여러번 선언
+        // 전달되는 자료형에 따라 메서드 이름은 똑같지만 전달값이 다른 메서드를 각각 호출함
+        // 메서드 오버로딩 : 같은 이름의 메서드를 여러번 선언
         // 전달값의 타입이 다르거나
         // 전달값의 개수가 다르거나
         // 반환형이 다른 타입으로는 X
