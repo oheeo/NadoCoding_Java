68c09f5f18d47801e420bf9941773a4b07bf4ad4
dahee oh <ekgml4122@gmail.com>
2023-07-15 23:20:06 +0900

    변수의 범위

--- src/chap_06/_07_VariableScope.java
+++ src/chap_06/_07_VariableScope.java
@@ -3,10 +3,12 @@ package chap_06;
 public class _07_VariableScope {
 
     public static void methodA(int number) {
+        // System.out.println(result);
+        System.out.println(number);  // 디폴트값인 0 출력
     }
 
     public static void methodB() {
-        int result = 1;
+        int result = 1;  // 지역 변수
     }
 
     public static void main(String[] args) {
@@ -16,11 +18,14 @@ public class _07_VariableScope {
             System.out.println(i);
         }
 
+        // System.out.println(i);  // for문 내에서 선언된 변수 i는 for문 내에서만 사용 가능
+
         {
             int j = 0;
             System.out.println(j);
             System.out.println(number);
         }
+        // System.out.println(j);
     }
 
 }
